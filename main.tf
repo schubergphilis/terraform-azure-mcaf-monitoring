651a4740380ca14519eb83695e9d5c2427380808
Dennis van der Meulen <dvandermeulen@schubergphilis.com>
2025-01-24 10:08:42 +0100

    add optional immutability policy

--- main.tf
+++ main.tf
@@ -72,13 +72,6 @@ resource "azurerm_storage_management_policy" "this" {
   }
 }
 
-# resource "azurerm_storage_container_immutability_policy" "example" {
-#   storage_container_resource_manager_id = azurerm_storage_container.example.resource_manager_id
-#   immutability_period_in_days           = 14
-#   protected_append_writes_all_enabled   = false
-#   protected_append_writes_enabled       = true
-# }
-
 resource "azurerm_log_analytics_data_export_rule" "this" {
   count = var.storage_account.enable_law_data_export ? 1 : 0
 
